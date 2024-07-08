0260f1796eb32d669b7ac3a6159b4e5ffc724fe7
androcider <97479559+androcider@users.noreply.github.com>
2024-07-08 12:24:40 -0600

    commit

---
+++ s24-prove-06-androcider-main/Translator.cs
@@ -0,0 +1,38 @@
+using System.Runtime.InteropServices.ComTypes;
+
+namespace prove_06;
+
+public class Translator {
+    private Dictionary<string, string> _words = new();
+
+    /// <summary>
+    /// Add the translation from 'from_word' to 'to_word'
+    /// For example, in a english to german dictionary:
+    /// 
+    /// my_translator.AddWord("book","buch")
+    /// </summary>
+    /// <param name="fromWord">The word to translate from</param>
+    /// <param name="toWord">The word to translate to</param>
+    /// <returns>fixed array of divisors</returns>
+    public void AddWord(string fromWord, string toWord)
+    {
+        var word = fromWord;
+        var translation = toWord;
+        _words.Add(word, translation);
+    }
+
+    /// <summary>
+    /// Translates the from word into the word that this stores as the translation
+    /// </summary>
+    /// <param name="fromWord">The word to translate</param>
+    /// <returns>The translated word or "???" if no translation is available</returns>
+    public string Translate(string fromWord)
+    {
+        var translation = "";
+        if (_words.ContainsKey(fromWord))
+            translation = _words[fromWord];
+        else
+            translation = "???";
+        return translation;
+    }
+}
